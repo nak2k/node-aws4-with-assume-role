26f1fde65fb861157571e8ffb4f388de736e306f
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-09-16 15:12:18 +0900

    Add tests

--- test/test.js
+++ test/test.js
@@ -2,10 +2,19 @@ const test = require('tape');
 const { makeSigner } = require('..');
 
 test('test', t => {
-  t.plan(2);
+  t.plan(4);
 
   makeSigner({}, (err, sign) => {
     t.error(err);
     t.equal(typeof sign, 'function');
+
+    const options = sign({
+      service: 's3',
+      path: '/',
+      signQuery: true,
+    });
+
+    t.equal(typeof options.service, 'string');
+    t.equal(typeof options.headers, 'object');
   });
 });
